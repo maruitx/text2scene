ee7442cc68f2bf8af085c86c110f8c56e8282dbf
Matt F <techmatt@gmail.com>
2016-12-19 12:27:47 -0800

    checking in mLib

---
+++ matt-SEL-playground/mLib/include/mlibeigen.h
@@ -0,0 +1,15 @@
+
+#pragma once
+#ifndef _MLIB_EIGEN_H_
+#define _MLIB_EIGEN_H_
+
+#include "Eigen33b1/Sparse"
+#include "Eigen33b1/Eigenvalues"
+
+//
+// ext-eigen headers
+//
+#include "ext-eigen/eigenUtility.h"
+#include "ext-eigen/eigenSolver.h"
+
+#endif // _MLIB_EIGEN_H_
