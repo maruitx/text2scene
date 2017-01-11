05ddb459f6e6ddb5b17e4422bb8b2d5cdc0f8a86
Matt F <techmatt@gmail.com>
2017-01-11 09:27:28 -0800

    More chang scenes.

--- data/changText2Scene/parser.js
+++ data/changText2Scene/parser.js
@@ -1,9 +1,14 @@
-scene=<text>
+function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
 output="StanfordSceneDatabase\n";
 output += "modelCount " + scene.length + "\n";
 for(const o of scene) {
 output += "newModel " + o.index + " " + o.modelId.replace("wss.", "") + "\n";
-output += "transform " + o.transform.data + "\n";
+output += "transform " + replaceAll(o.transform.data.toString(), ",", " ") + "\n";
 output += "position 0 0 0" + "\n";
 output += "frontDir 1 0 0" + "\n";
 output += "upDir 0 0 1" + "\n";
